e3b624fea8295a5d502f206a95bc27737bdf94eb
Leonid Vinogradov <leonid.52669@gmail.com>
2019-01-19 18:36:05 +0300

    antlr hello world

--- backend/src/main/java/ru/startupbase/Main.java
+++ backend/src/main/java/ru/startupbase/Main.java
@@ -1,17 +1,85 @@
 package ru.startupbase;
 
-import static java.text.MessageFormat.format;
-import java.util.Arrays;
-import java.util.Date;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import static ru.startupbase.JettyServer.createServer;
+import ru.startupbase.antlr4.Java8BaseListener;
+import ru.startupbase.antlr4.Java8Lexer;
+import ru.startupbase.antlr4.Java8Parser;
 import ru.startupbase.config.ProdConfig;
+import org.antlr.v4.runtime.CharStreams;
+
+import java.util.*;
+
+import static java.text.MessageFormat.format;
+import static ru.startupbase.JettyServer.createServer;
 
 public class Main {
 
   public static void main(String[] args) {
+
+    class UppercaseMethodListener extends Java8BaseListener {
+
+      private List<String> errors = new ArrayList<>();
+
+      // ... getter for errors
+      public List<String> getErrors(){
+        return Collections.unmodifiableList(errors);
+      }
+
+      @Override
+      public void enterMethodDeclarator(Java8Parser.MethodDeclaratorContext ctx) {
+        TerminalNode node = ctx.Identifier();
+        String methodName = node.getText();
+
+        if (Character.isUpperCase(methodName.charAt(0))) {
+          String error = String.format("Method %s is uppercased!", methodName);
+          errors.add(error);
+        }
+      }
+    }
+
+    String javaClassContent = "public class SampleClass { void doSomething(){int a = 4; int b = 5; int c = a + b;} }";
+    Java8Lexer java8Lexer = new Java8Lexer(CharStreams.fromString(javaClassContent));
+
+    CommonTokenStream tokens = new CommonTokenStream(java8Lexer);
+    Java8Parser parser = new Java8Parser(tokens);
+    ParseTree tree = parser.compilationUnit();
+
+    ParseTreeWalker walker = new ParseTreeWalker();
+    UppercaseMethodListener listener= new UppercaseMethodListener();
+
+    walker.walk(listener, tree);
+
+    //System.out.println(listener.getErrors().get(0));
+
+    System.out.println(tree.toStringTree(parser));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    System.out.println("done");
+    System.exit(0);
+
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProdConfig.class);
 
     try {
